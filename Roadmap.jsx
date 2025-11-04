a1af3b4b9b699ac6030a025006c28b241c4fb599
Ayush Sharma <as3848274@gmail..com>
2025-11-05 00:13:19 +0530

    first react commit

---
+++ Roadmap.jsx
@@ -0,0 +1,22 @@
+// Why to learn React ??
+
+// --> job,trend, UI
+// --> makes easy to manage & build complex frontend
+
+
+
+//Why should i learn react ?
+
+// --> After mastering JS 
+// --> most project don't need react in initial phase
+
+
+// babel  -  Js compiler used to convert js code written using latest features to older version of js or old js syntax so we write the js code using all the latest feature and then convert it into old js syntax using babel because not all browsers supports Ecmascript6 and Ecmascript7 syntax and no browser will support JSX syntax.. 
+
+// babel is a tool basically used to convert ECMAscript6 syntax into ECMAscript5. It is also grew to support transpling JSX syntax into pure React..
+
+
+// React is a library !!
+// Framework - miliatry
+// library - cool dude (freedom)
+
